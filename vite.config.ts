1bc1218c709ebdd6562deaf01861754ee4bbf002
David Kellerman <david@silvermoon.org>
2023-03-02 18:20:05 -0500

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
@@ -8,14 +7,6 @@ import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/pages/index.vue'),
-        sandbox: resolve(__dirname, 'src/pages/sandbox.vue'),
-      },
-    },
-  },
   plugins: [
     Vue(),
     AutoImport({
@@ -34,7 +25,9 @@ export default defineConfig({
       dts: true,
     }),
     Pages({
-      dirs: ['src/pages'],
+      dirs: [
+        { dir: 'src/pages', baseRoute: '' },
+      ],
     }),
   ],
 });
