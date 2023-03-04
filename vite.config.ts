70842341342e56ebc3c0bd33aa60806b0796b594
David Kellerman <david@silvermoon.org>
2023-03-04 10:51:26 -0500

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,31 @@ import AutoImport from 'unplugin-auto-import/vite';
 import Pages from 'vite-plugin-pages';
 import Components from 'unplugin-vue-components/vite';
 
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    Vue(),
-    AutoImport({
-      dts: './src/auto-imports.d.ts',
-      imports: [
-        'vue',
-        'vue-router',
-        'pinia',
-        '@vueuse/core',
-      ],
-      dirs: ['./src/**'],
-      cache: false,
-      vueTemplate: true,
-    }),
-    Components({
-      dts: true,
-    }),
-    Pages({
-      dirs: [
-        { dir: 'src/pages', baseRoute: '' },
-      ],
-    }),
-  ],
+export default defineConfig(() => {
+  return {
+    plugins: [
+      Vue(),
+      AutoImport({
+        dts: './src/auto-imports.d.ts',
+        imports: [
+          'vue',
+          'vue-router',
+          'pinia',
+          '@vueuse/core',
+        ],
+        dirs: ['./src/**'],
+        cache: false,
+        vueTemplate: true,
+      }),
+      Components({
+        dts: true,
+      }),
+      Pages({
+        dirs: [
+          { dir: 'src/pages', baseRoute: '' },
+        ],
+      }),
+    ],
+  };
 });
